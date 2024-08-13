891861ffe70d2465129dd1fead1dc67e00d9a1b6
Guizão <guilhermeeufrasiobarbosa@gmail.com>
2024-08-12 21:00:52 -0300

    Create exercicio4.py
    adicionado o exercício 4

---
+++ exercicio4.py
@@ -0,0 +1,8 @@
+n1= int(input("Digite o 1° número: "))
+n2= int(input("Digite o 2° número: "))
+if (n1>n2):
+    print ("O maior número é o primeiro")
+elif (n1<n2):
+    print("O maior número é o segundo")
+else:
+    print ("Os números são iguais")
