28a433c0b5660613630419b2aaf866862010ba7c
Guizão <guilhermeeufrasiobarbosa@gmail.com>
2024-08-13 20:13:05 -0300

    Create exercicio8.py
    adicionado o exercício 8

---
+++ exercicio8.py
@@ -0,0 +1,5 @@
+letra= input("Digite uma letra: ")
+if (letra=='a'or letra=='e'or letra=='i'or letra=='o'or letra=='u'):
+       print("A letra que foi digitada é uma vogal")
+else:
+       print("A letra que foi digitada é uma consoante")
