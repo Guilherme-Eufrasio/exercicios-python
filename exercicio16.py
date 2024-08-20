2a08a70d7aaa16327fdc70ab0c71c9ce1b703283
Guizão <guilhermeeufrasiobarbosa@gmail.com>
2024-08-20 18:32:46 -0300

    Create exercicio16.py
    adicionado o exercício 16

---
+++ exercicio16.py
@@ -0,0 +1,4 @@
+sala= int(input("Digite o seu salário: "))
+aumento= int(input("Digite a porcentagem do seu aumento: "))
+nvsala= sala+(sala*aumento/100)
+print (f"O seu novo salário é de {nvsala} reais")
