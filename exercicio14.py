1e55c27377db1b5d7ec57e2b4e80a851ad9f1305
Guizão <guilhermeeufrasiobarbosa@gmail.com>
2024-08-19 21:03:19 -0300

    Create exercicio14.py
    adicionado o exercício 14

---
+++ exercicio14.py
@@ -0,0 +1,7 @@
+letra=input("Digite seu gênero. EX: M ou F: ")
+if (letra=='M' or letra=='m'):
+  print("Seu gênero é masculino")
+elif(letra=='F' or letra=='f'):
+  print("Seu gênero é feminino")
+else:
+  print("Sexo inválido")
