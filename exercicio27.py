5eade6304ae5b9c56bf79d6d365f7e868e04f6f4
Guizão <guilhermeeufrasiobarbosa@gmail.com>
2024-08-21 20:59:38 -0300

    Create exercicio27.py
    adicionado o exercício 27

---
+++ exercicio27.py
@@ -0,0 +1,16 @@
+anoN=int(input ("Digite o ano do seu nascimento: "))
+anoA= 2024
+ano19= 2019
+dias= 365
+meses= 12
+semanas= 52
+idade= anoA-anoN
+idade19= ano19-anoN
+idadeD= anoN*dias
+idadeM= anoN*meses
+idadeS= anoN*semanas
+print (f"\nVocê tem {idade} anos")
+print(f"\nSua idade em 2019 foi de {idade19} anos")
+print(f"\nSua idade em dia é de {idadeD} dias")
+print(f"\nSua idade em meses é de {idadeM} meses")
+print(f"\nSua idade em semanas é de {idadeS} semanas")
