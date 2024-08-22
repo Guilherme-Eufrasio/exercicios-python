e0323aca3980200beeb43683820161dd6539fdaa
Guizão <guilhermeeufrasiobarbosa@gmail.com>
2024-08-21 21:01:13 -0300

    Create exercicio30.py
    adicionado o exercício 30

---
+++ exercicio30.py
@@ -0,0 +1,17 @@
+n1= int(input("Digite o 1° número: "))
+n2= int(input("Digite o 2° número: "))
+oprd = input("Digite um sinal para a conta: ")
+if (oprd== '+'):
+  resultado= n1+n2
+  print (f"O resultado da conta foi {resultado}")
+elif (oprd== '-'):
+  resultado= n1-n2
+  print(f"O resultado da conta foi de {resultado}")
+elif (oprd== 'x' or oprd== 'X' or oprd== '*'):
+  resultado= n1*n2
+  print(f"O resultado da conta foi de {resultado}")
+elif (oprd== '/'):
+  resultado= n1/n2
+  print(f"O resultado da conta foi de {resultado}")
+else:
+  print("Operação inválida.")
