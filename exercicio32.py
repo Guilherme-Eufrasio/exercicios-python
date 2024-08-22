fa591139e44b5225dbdfba5387f95e3f3684e4b2
Guizão <guilhermeeufrasiobarbosa@gmail.com>
2024-08-21 21:02:22 -0300

    Create exercicio32.py
    adicionado o exercício 32

---
+++ exercicio32.py
@@ -0,0 +1,4 @@
+graus= float(input("Digite os graus para converter para Fahrenheint e Kelvin: "))
+fahren= (graus*9/5)+32
+kelvin= graus+273.15
+print (f"A conversão para Fahrenheit foi de {fahren} e de Kelvin foi de {kelvin}")
