3f5dbc56f1f0810ba70b6a19631fd11e34eef3c8
Guizão <guilhermeeufrasiobarbosa@gmail.com>
2024-08-21 21:00:42 -0300

    Create exercicio29.py
    adicionado o exercício 29

---
+++ exercicio29.py
@@ -0,0 +1,5 @@
+n1=int(input("Digite o 1° número: "))
+n2=int(input("Digite o 2° número: "))
+n3=int(input("Digite o 3° número: "))
+mult=n1*n2*n3
+print(f"O resultado da multiplicação desses números foi de {mult}")
