7fd2924824b230028e37dcf223d328398e05a3cd
Guizão <guilhermeeufrasiobarbosa@gmail.com>
2024-08-26 20:45:33 -0300

    Create exercicio36.py
    adicionado o exercício 36

---
+++ exercicio36.py
@@ -0,0 +1,18 @@
+num= int(input("Digite um número: "))
+if (num==1):
+  print("1-Segunda-Feira")
+elif(num==2):
+  print("2-Terça-Feira")
+elif(num==3):
+  print("3-Quarta-Feira")
+elif(num==4):
+  print("4-Quinta-Feira")
+elif(num==5):
+  print("5-Sexta-Feira")
+elif(num==6):
+  print("Sábado")
+elif(num==7):
+  print("Domingo")
+elif(num>=8):
+  print ("Digite outro número")
+  
