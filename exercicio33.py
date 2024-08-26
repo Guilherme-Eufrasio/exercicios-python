5da5bde99a5d9411c5fddb1e42552006bd2e9da4
Guizão <guilhermeeufrasiobarbosa@gmail.com>
2024-08-26 18:38:48 -0300

    Create exercicio33.py
    adicionado o exercício 33

---
+++ exercicio33.py
@@ -0,0 +1,7 @@
+n1= int(input("Digite o 1° número: "))
+n2= int(input("Digite o 2° número"))
+nr= float(input("Digite um número com vírgula.\n EX 1.1\n"))
+resultado1= (n1*2)+n2/2
+resultado2= (n1*3)+n2
+resultado3= n2**3
+print (f"Alguns cálculos desses números são: {resultado1}, {resultado2} e {resultado3}")
