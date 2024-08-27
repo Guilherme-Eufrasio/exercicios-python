c8996543c7cb4b2fa8f4b17527edcb98ee6ccd17
Guizão <guilhermeeufrasiobarbosa@gmail.com>
2024-08-27 20:39:25 -0300

    Create exercicio42.py
    adicionado o exercício 42

---
+++ exercicio42.py
@@ -0,0 +1,8 @@
+n1= float(input("Digite o 1° número: "))
+n2= float(input("Digite o 2° número: "))
+n3= float(input("Digite o 3° número: "))
+n4= float(input("Digite o 4° número: "))
+n5= float(input("Digite o 5° número: "))
+media = (n1+n2+n3+n4+n5)/5
+soma = n1+n2+n3+n4+n5
+print (f"A média desses números são de {media} e a soma é de {soma}")
