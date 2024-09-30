64f7c07b21e6973f23b1d123672684d8fe288e86
Guizão <guilhermeeufrasiobarbosa@gmail.com>
2024-09-30 20:59:57 -0300

    Create exercicio62.py
    adicionado o exercício 62

---
+++ exercicio62.py
@@ -0,0 +1,7 @@
+i=0
+qtdA=int(input("Quantos alunos tem na sala do 2DS?\n:"))
+for i in range (i,qtdA):
+  nomeA=input(f"Digite o nome do {i+1}° aluno: ")
+# for i in range (i,qtdA):
+print(f"\n", nomeA)
+print("\n")
