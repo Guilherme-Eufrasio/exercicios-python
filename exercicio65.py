d68cae720eb005ffb5dae874b883f190bea3da90
Guizão <guilhermeeufrasiobarbosa@gmail.com>
2024-09-30 21:02:13 -0300

    Create exercicio65.py
    adicionado o exercício 65

---
+++ exercicio65.py
@@ -0,0 +1,9 @@
+i=0
+num=[]
+for i in range (i,10):
+  num.append(int(input(f"Digite o {i+1}° número: ")))
+  
+maiorn= max(num)
+print (f"O maior número é {maiorn}")
+menorn= min(num)
+print (f"O menor número é {menorn}")
